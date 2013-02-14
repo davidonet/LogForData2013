0df2192fe71538ac6e056eb918b17b0d5f57549a
David Olivari <david.olivari@gmail.com>
2013-02-14 11:27:55 +0100

    dat.gui control prepared

--- js/htmldecoder.js
+++ js/htmldecoder.js
@@ -1,15 +1,29 @@
-define(["dyn"], function(dyn) {
+define(["dyn", "lib/dat.gui.min"], function(dyn, GUI) {
 	return {
 		init : function() {
-			var localDiv = $('<div id="rawtext">');
-			var pos = 0;
-			localDiv.load('/data/Text_test_Kawenga.txt');
-			$('#testtxt').click(function() {
-				$('#render').html(localDiv[0].innerHTML.slice(pos, pos + 80));
-				pos += 80;
-			});
-			$('#updatetag').click(dyn.updateTag);
-
+			var VisualEngine = function() {
+				this.original = 50;
+				this.inline = 50;
+				this.asciimod = 30;
+				this.wordmod = 20;
+				this.wordrange = 3;
+				this.blockinsert = 10;
+				this.clean = function() {
+					$('#render').empty();
+				}
+			}
+			var aVE = new VisualEngine();
+			var gui = new dat.GUI();
+			gui.remember(aVE);
+			gui.add(aVE, 'original', 0, 100);
+			gui.add(aVE, 'blockinsert', 1, 100);	
+			gui.add(aVE, 'inline', 0, 100);
+			gui.add(aVE, 'asciimod', 0, 100);
+			gui.add(aVE, 'wordmod', 0, 100);
+			gui.add(aVE, 'wordrange', 1, 10);
+			gui.add(aVE, 'clean');
+			
+			
 			$('#render').on('receive', function(event, text) {
 				var changeChance = .8;
 				var changeBigChance = .5;
@@ -22,7 +36,7 @@ define(["dyn"], function(dyn) {
 				alea = Math.random();
 				if (alea < changeChance) {
 					var c = Math.floor(Math.random() * 100) % 8;
-					$(elt).addClass('c' + c);
+					$(elt).addClass('style' + c);
 				} else {
 					$('#render').append("<hr/>");
 				}
@@ -31,25 +45,6 @@ define(["dyn"], function(dyn) {
 				dyn.updateTag();
 				$('#render').scrollTop(65000);
 			})
-
-			$('#updatetext').click(function() {
-				var changeChance = .5;
-				var changeBigChance = .5;
-				var alea = Math.random();
-				var elt;
-				if (alea < changeBigChance)
-					elt = document.createElement('div');
-				else
-					elt = document.createElement('span');
-				alea = Math.random();
-				if (alea < changeChance) {
-					var c = Math.floor(Math.random() * 100) % 8;
-					$(elt).addClass('c' + c);
-				}
-				$(elt).html($('#txt').text());
-				$('#render').append(elt);
-				$('#txt').empty();
-			});
 		}
 	}
 });
